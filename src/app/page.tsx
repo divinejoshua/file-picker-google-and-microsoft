da719a4cb8a6670f7ceab466a2e3f32f5519d817
divinejoshua <divineuche399@gmail.com>
2025-08-05 11:03:39 +0100

    Added google drive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client"
+import  { useEffect } from 'react';
+import useDrivePicker from 'react-google-drive-picker'
 
 export default function Home() {
+
+  const [openPicker, authResponse] = useDrivePicker();  
+  // const customViewsArray = [new google.picker.DocsView()]; // custom view
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      developerKey: process.env.NEXT_PUBLIC_GOOGLE_DEVELOPER_KEY!,
+      viewId: "DOCS",
+      // token: token, // pass oauth token in case you already have one
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      multiselect: true,
+      // customViews: customViewsArray, // custom view
+      callbackFunction: (data) => {
+        if (data.action === 'cancel') {
+          console.log('User clicked cancel/close button')
+        }
+        console.log(data)
+      },
+    })
+  }
+
+
+
   return (
     <div>
       <h1>File Picker</h1>
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleOpenPicker}>Google Picker</button>
     </div>
   );
 }
